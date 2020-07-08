d9bb86dfa53c0676c0eb2d214aecc1693c56655a
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-08 23:58:16 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,6 +6,7 @@ def count_strings(array)
     end
     index += 1
 end
+end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
