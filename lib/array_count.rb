8bca18f1c85d3665abd4c8e54e40d5c3fdf11fc4
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-09 00:15:22 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,9 +1,9 @@
 def count_strings(array)
-  array.count do |s|
-    s == ""
-  end
+  
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count do |s|
+    s == ""
+  end
 end
