86e7e013817eac92fe0dcf2f95624ca40f2c4440
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-09 00:30:33 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,11 @@
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+  array.count do |a|
+    a.class == String
+  end
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count do |s|
+    s == ""
+  end
 end
